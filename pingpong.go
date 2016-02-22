cc6b29fd4a160571f9b088eb915a8dfdc9cbd577
Matt Bridges <mbridges.91@gmail.com>
2016-02-22 09:05:38 -0600

    Adds pingpong.go

---
+++ pingpong.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ponger(c chan string) {
+	for {
+		select {
+		case msg := <-c:
+			switch msg {
+			case "ping":
+				fmt.Println("pong")
+			case "pong":
+				fmt.Println("ping")
+			}
+		}
+	}
+}
+
+func asker(pinger chan<- string, done chan<- bool) {
+	var input string
+	fmt.Scanln(&input)
+	switch input {
+	case "ping", "pong":
+		pinger <- input
+		asker(pinger, done)
+	case "exit":
+		done <- true
+	}
+}
+
+func main() {
+	done := make(chan bool, 1)
+	channel := make(chan string, 1)
+
+	go ponger(channel)
+
+	asker(channel, done)
+	<-done
+}
